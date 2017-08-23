1ae368c88c5208dfe16bd527b5f087341f3c46c5
f763566485@qq.com <wen123456>
2017-08-23 11:41:06 +0800

    Initial commit

---
+++ app/src/main/java/com/leafact/fingerprintdemo/MainActivity.java
@@ -0,0 +1,128 @@
+package com.leafact.fingerprintdemo;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.hardware.fingerprint.FingerprintManagerCompat;
+import android.support.v4.view.LayoutInflaterFactory;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+    }
+    public void onFingerprintClick(View v){
+
+        FingerprintUtil.callFingerPrint(new FingerprintUtil.OnCallBackListenr() {
+            AlertDialog dialog;
+            @Override
+            public void onSupportFailed() {
+                showToast("当前设备不支持指纹");
+            }
+
+            @Override
+            public void onInsecurity() {
+                showToast("当前设备未处于安全保护中");
+            }
+
+            @Override
+            public void onEnrollFailed() {
+                showToast("请到设置中设置指纹");
+            }
+
+            @Override
+            public void onAuthenticationStart() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                View view = LayoutInflater.from(mContext).inflate(R.layout.layout_fingerprint,null);
+                initView(view);
+                builder.setView(view);
+                builder.setCancelable(false);
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.removeMessages(0);
+                        FingerprintUtil.cancel();
+                    }
+                });
+                dialog = builder.create();
+                dialog.show();
+            }
+
+            @Override
+            public void onAuthenticationError(int errMsgId, CharSequence errString) {
+                showToast(errString.toString());
+                if (dialog != null  &&dialog.isShowing()){
+                    dialog.dismiss();
+                    handler.removeMessages(0);
+                }
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                showToast("解锁失败");
+            }
+
+            @Override
+            public void onAuthenticationHelp(int helpMsgId, CharSequence helpString) {
+                showToast(helpString.toString());
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(FingerprintManagerCompat.AuthenticationResult result) {
+                showToast("解锁成功");
+                if (dialog != null  &&dialog.isShowing()){
+                    dialog.dismiss();
+                    handler.removeMessages(0);
+                }
+
+            }
+        });
+    }
+    private Handler handler= new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 0){
+                int i = postion % 5;
+                if (i == 0){
+                    tv[4].setBackground(null);
+                    tv[i].setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                }
+                else{
+                     tv[i].setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                     tv[i-1].setBackground(null);
+                }
+                postion++;
+                handler.sendEmptyMessageDelayed(0,100);
+            }
+        }
+    };
+    TextView[] tv = new TextView[5];
+    private int postion = 0;
+    private void initView(View view) {
+        postion = 0;
+        tv[0] = (TextView) view.findViewById(R.id.tv_1);
+        tv[1] = (TextView) view.findViewById(R.id.tv_2);
+        tv[2] = (TextView) view.findViewById(R.id.tv_3);
+        tv[3] = (TextView) view.findViewById(R.id.tv_4);
+        tv[4] = (TextView) view.findViewById(R.id.tv_5);
+        handler.sendEmptyMessageDelayed(0,100);
+    }
+
+
+    public void showToast(String name ){
+        Toast.makeText(MainActivity.this,name,Toast.LENGTH_SHORT).show();
+    }
+}
